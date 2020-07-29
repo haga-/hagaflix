95d773264f2c088fddbd582f9d6b28dc2d0f3210
Heitor Almeida <heitor.haga@gmail.com>
2020-07-29 19:09:39 -0300

    Aula2

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './components/pages/Home';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CadastroCategoria from './components/pages/Categoria';
+import CadastroVideo from './components/pages/Video';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact></Route>
+      <Route path="/cadastro/video" component={CadastroVideo}></Route>
+      <Route path="/cadastro/categoria" component={CadastroCategoria}></Route>
+      <Route component={() => (<div>404</div>)}></Route>
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
