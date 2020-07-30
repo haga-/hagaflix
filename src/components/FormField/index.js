a55371b684dc1e916fd351c63f7bdd1ae60bcbce
Heitor Almeida <heitor.haga@gmail.com>
2020-07-30 11:10:18 -0300

    Aula3

---
+++ src/components/FormField/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function FormField({ label, type, name, value, onChange }) {
+  return (
+    <div>
+      <label>
+        {label}:
+        {type === 'textarea' &&
+          <textarea
+            value={value}
+            name={name}
+            onChange={onChange}>
+          </textarea>
+        }
+        {
+          type !== 'textarea' &&
+          <input
+           type={type}
+           value={value}
+           name={name}
+           onChange={onChange}
+          />
+        }
+      </label>
+    </div>
+  )
+}
+
+export default FormField;
